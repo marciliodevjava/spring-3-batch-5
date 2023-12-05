6c67f13c1ef26ba5ca57e827d24ee42262647b2b
marcsilva <marciliodevjava@gmail.com>
2023-12-05 17:55:36 -0300

    Update BatchConfiguration

--- src/main/java/com/example/batch/job/BatchConfiguration.java
+++ src/main/java/com/example/batch/job/BatchConfiguration.java
@@ -7,6 +7,7 @@ import org.springframework.batch.core.launch.JobLauncher;
 import org.springframework.batch.core.repository.JobRepository;
 import org.springframework.batch.core.step.builder.StepBuilder;
 import org.springframework.batch.core.step.tasklet.Tasklet;
+import org.springframework.batch.item.*;
 import org.springframework.batch.repeat.RepeatStatus;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.ApplicationRunner;
@@ -69,6 +70,25 @@ public class BatchConfiguration {
             var instanceId = run.getJobInstance().getInstanceId();
             System.out.println("instanceId: " + instanceId);
         };
+    }
+
+    @Bean
+    Step csvToDb(JobRepository jobRepository, PlatformTransactionManager transactionManager){
+        return new StepBuilder("csvToDb", jobRepository)
+                .<String,String>chunk(100, transactionManager)
+                .reader(new ItemReader<String>() {
+                    @Override
+                    public String read() throws Exception, UnexpectedInputException, ParseException, NonTransientResourceException {
+                        return null;
+                    }
+                })
+                .writer(new ItemWriter<String>() {
+                    @Override
+                    public void write(Chunk<? extends String> chunk) throws Exception {
+
+                    }
+                })
+                .build();
     }
 
     @Bean
